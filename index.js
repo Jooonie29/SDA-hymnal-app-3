d823233ff8d750a8b8aaac74c533cd3073ff4905
Jooonie29 <highscoredsns@gmail.com>
2025-12-04 17:15:19 +0800

    Delete index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-import TrackPlayer from 'react-native-track-player';
-TrackPlayer.registerPlaybackService(() => require('./app/services/trackPlayerService').default);
-export { default } from './App';
-
